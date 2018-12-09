50a1e8d5db4566652752c8b5b1a418ab96d9daa3
Ehsan Khodabandeh <ehsan.khodabandeh@opexanalytics.com>
2018-12-08 20:17:47 -0600

    first commit for solving the problem with docplex

---
+++ execute_docplex.py
@@ -0,0 +1,133 @@
+#!/usr/bin/env python
+
+import logging
+from time import time
+
+import docplex.mp.model as cpx
+
+import helper
+import process_data
+from parameters import model_params
+
+__author__ = 'Ehsan Khodabandeh'
+__version__ = '1.0'
+# ====================================
+
+LOG_FORMAT = '%(asctime)s  %(name)-12s %(levelname)s : %(message)s'
+logging.basicConfig(format=LOG_FORMAT, level=logging.INFO)
+# Since the level here is INFO, all the logger.debug below are not displayed
+logger = logging.getLogger(__name__ + ': ')
+
+# ================== Set up data ==================
+input_df_dict, input_param_dict = process_data.load_data()
+logger.info('Data is loaded!')
+
+# ================== Set up the optimization model ==================
+"""
+Parameters:
+h: unit holding cost
+p: production capacity per month
+I_0: initial_inventory
+c_t: unit production cost in month t
+d_t: demand of month t
+
+Variables:
+X_t: Amount produced in month t
+I_t: Inventory at the end of period t
+
+Constraints:
+Inventory Constraints: I_{t-1} + X_t - d_t = I_t
+Capacity Constraints: X_t <= p
+
+Objective: Min Sum(h*I_t + c_t*X_t)
+"""
+
+model = cpx.Model('prod_planning')
+
+start = time()
+
+# ================== Decision variables ==================
+production_variables = model.continuous_var_dict(input_df_dict['input_data'].index, name="X")
+inventory_variables = model.continuous_var_dict(input_df_dict['input_data'].index, name="I")
+
+# Alternative way of creating the variables
+# production_variables = {index: model.continuous_var(name='X_' + str(row['period']))
+#                         for index, row in input_df_dict['input_data'].iterrows()}
+#
+# inventory_variables = {index: model.continuous_var(name='I_' + str(row['period']))
+#                        for index, row in input_df_dict['input_data'].iterrows()}
+
+logger.debug("var declaration time: {:.6f}".format(time() - start))
+
+# ================== Inventory balance constraints ==================
+inv_balance_constraints = model.add_constraints(
+    (inventory_variables[period - 1] + production_variables[period]
+     - inventory_variables[period] == value.demand,
+     'inv_balance' + str(period))
+    for period, value in input_df_dict['input_data'].iloc[1:].iterrows())
+
+# inv balance for first period
+first_period_inv_balance_constraints = model.add_constraint(
+    ct=production_variables[0] - inventory_variables[0]
+       == input_df_dict['input_data'].iloc[0].demand - input_param_dict['initial_inventory'],
+    ctname='inv_balance0')
+
+# ================== Production capacity constraints ==================
+production_capacity_constraints = model.add_constraints(
+    (value <= input_df_dict['input_data'].iloc[index].production_capacity,
+     'prod_cap_month_' + str(index))
+    for index, value in production_variables.items())
+
+"""
+Following is an alternative way of defining 2 of the above 3 constraints.
+Check for yourself the difference between the two.
+
+# ================== Inventory balance constraints ==================
+# Version 2
+inv_balance_constraints = {
+    period: model.add_constraint(
+        ct=inventory_variables[period - 1] + production_variables[period]
+           - inventory_variables[period] == value.demand,
+        ctname='inv_balance' + str(period))
+    for period, value in input_df_dict['input_data'].iloc[1:].iterrows()}
+
+# inv balance for first period
+first_period_inv_balance_constraints = model.add_constraint(
+    ct=production_variables[0] - inventory_variables[0]
+       == input_df_dict['input_data'].iloc[0].demand - input_param_dict['initial_inventory'],
+    ctname='inv_balance0')
+
+# ================== Production capacity constraints ==================
+production_capacity_constraints = {
+    index: model.add_constraint(
+        ct=value <= input_df_dict['input_data'].iloc[index].production_capacity,
+        ctname='prod_cap_month_' + str(index))
+    for index, value in production_variables.items()}"""
+
+# ================== Costs and objective function ==================
+total_holding_cost = input_param_dict['holding_cost'] * model.sum(inventory_variables)
+total_production_cost = model.sum(row['production_cost'] * production_variables[index]
+                                  for index, row in input_df_dict['input_data'].iterrows())
+
+objective = total_holding_cost + total_production_cost
+
+model.minimize(objective)
+logger.info('Model creation time in sec: {:.4f}'.format(time() - start))
+
+# ================== Optimization ==================
+if model_params['write_lp']:
+    logger.info('Writing the lp file!')
+    model.export_as_lp('./{}.lp'.format(model.name))
+
+logger.info('Optimization starts!')
+if model.solve():
+    logger.info('The solution is optimal and the objective value '
+                'is ${:,.2f}!'.format(model.objective_value))
+
+# ================== Output ==================
+dict_of_variables = {'production_variables': production_variables,
+                     'inventory_variables': inventory_variables}
+
+output_df = process_data.write_outputs(dict_of_variables, attr='solution_value')
+helper.write_to_csv(output_df)
+logger.info('Outputs are written to csv!')
---
+++ optimization_model_docplex.py
@@ -0,0 +1,105 @@
+import logging
+
+import docplex.mp.model as cpx
+
+from helper import write_to_csv
+from parameters import model_params
+from process_data import write_outputs
+
+__author__ = 'Ehsan Khodabandeh'
+__version__ = '1.0'
+# ====================================
+
+logger = logging.getLogger(__name__ + ': ')
+
+
+class OptimizationModel(object):
+    def __init__(self, input_data, input_params):
+        self.input_data = input_data
+        self.input_params = input_params
+        self.model = cpx.Model('prod_planning')
+        self._create_decision_variables()
+        self._create_main_constraints()
+        self._set_objective_function()
+
+    # ================== Decision variables ==================
+    def _create_decision_variables(self):
+        self.production_variables = self.model.continuous_var_dict(self.input_data.index, name="X")
+        self.inventory_variables = self.model.continuous_var_dict(self.input_data.index, name="I")
+
+        # Alternative way of creating the variables
+        # self.production_variables = {index: self.model.continuous_var(name='X_' + str(row['period']))
+        #                              for index, row in self.input_data.iterrows()}
+        #
+        # self.inventory_variables = {index: self.model.continuous_var(name='I_' + str(row['period']))
+        #                             for index, row in self.input_data.iterrows()}
+
+    # ================== Constraints ==================
+    def _create_main_constraints(self):
+        # Depending on what you need, you may want to consider creating any of
+        # the expressions (constraints or objective terms) as an attribute of
+        # the OptimizationModel class (e.g. self.inv_balance_constraints).
+        # That way if, for example, at the end of the optimization you need to check
+        # the slack variables of certain constraints, you know they already exists in your model
+
+        # ================== Inventory balance constraints ==================
+        self.inv_balance_constraints = self.model.add_constraints(
+            (self.inventory_variables[period - 1] + self.production_variables[period]
+             - self.inventory_variables[period] == value.demand,
+             'inv_balance' + str(period))
+            for period, value in self.input_data.iloc[1:].iterrows())
+
+        # inv balance for first period
+        self.first_period_inv_balance_constraints = self.model.add_constraint(
+            ct=self.production_variables[0] - self.inventory_variables[0]
+               == self.input_data.iloc[0].demand - self.input_params['initial_inventory'],
+            ctname='inv_balance0')
+
+        # ================== Production capacity constraints ==================
+        self.production_capacity_constraints = self.model.add_constraints(
+            (value <= self.input_data.iloc[index].production_capacity,
+             'prod_cap_month_' + str(index))
+            for index, value in self.production_variables.items())
+
+    # ================== Costs and objective function ==================
+    def _set_objective_function(self):
+        # Similar to constraints, saving the costs expressions as attributes
+        # can give you the chance to retrieve their values at the end of the optimization
+        self.total_holding_cost = self.input_params['holding_cost'] * self.model.sum(self.inventory_variables)
+
+        self.total_production_cost = self.model.sum(row['production_cost'] * self.production_variables[index]
+                                                    for index, row in self.input_data.iterrows())
+
+        objective = self.total_holding_cost + self.total_production_cost
+        self.model.minimize(objective)
+
+    # ================== Optimization ==================
+    def optimize(self):
+        if model_params['write_lp']:
+            logger.info('Writing the lp file!')
+            self.model.export_as_lp('./{}.lp'.format(self.model.name))
+
+        logger.info('Optimization starts!')
+        if model_params['mip_gap']:
+            self.model.parameters.mip.tolerances.mipgap = model_params['mip_gap']
+        if model_params['time_limit']:
+            self.model.set_time_limit(model_params['time_limit'])
+
+        if model_params['write_log']:
+            with open("cplex.log", "w") as outs:
+                # prints CPLEX output to file "cplex.log"
+                self.model.solve(log_output=outs)
+        else:
+            self.model.solve(log_output=model_params['display_log'])
+
+        if self.model.solve_details.status == 'optimal':
+            logger.info('The solution is optimal and the objective value '
+                        'is ${:,.2f}!'.format(self.model.objective_value))
+
+    # ================== Output ==================
+    def create_output(self):
+        dict_of_variables = {'production_variables': self.production_variables,
+                             'inventory_variables': self.inventory_variables}
+
+        output_df = write_outputs(dict_of_variables, attr='solution_value')
+        write_to_csv(output_df)
